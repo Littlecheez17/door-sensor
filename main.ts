2d677733ba68b0ee078eefd5c610392a57a65198
Cheez <106701061+Littlecheez17@users.noreply.github.com>
2022-06-04 13:05:23 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+input.onPinPressed(TouchPin.P0, function () {
+    open = 0
+})
+input.onPinReleased(TouchPin.P0, function () {
+    open = 1
+    alarmOn = 1
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+    alarmOn = 0
+})
+let alarmOn = 0
+let open = 0
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
 basic.forever(function () {
-	
+    while (alarmOn == 1) {
+        music.setVolume(255)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+    }
 })
